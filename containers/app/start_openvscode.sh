81e399a9e6fa70ad430b8b74db87819e9f4f6375
Travis James <travis@tribehealthsolutions.com>
2025-05-01 11:14:03 -0500

    remote scenario

--- containers/app/start_openvscode.sh
+++ containers/app/start_openvscode.sh
@@ -6,7 +6,7 @@ OPENVSCODE_BIN="/openhands/.openvscode-server/bin/openvscode-server"
 
 if [ -x "$OPENVSCODE_BIN" ]; then
   echo "Starting OpenVSCode server on port $VSCODE_PORT..."
-  exec "$OPENVSCODE_BIN" --port "$VSCODE_PORT" --without-connection-token --disable-x-frame-options
+  exec "$OPENVSCODE_BIN" --port "$VSCODE_PORT" --without-connection-token --disable-x-frame-options --server-base-path vscode
 else
   echo "OpenVSCode server binary not found, skipping."
   exit 0
